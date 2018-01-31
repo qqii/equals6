ea01a8f373bbece173c7d7f262241bf62dbaaab5
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-01-31 13:21:40 +0000

    Basic UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 "use strict"
+var output;
 
-var button = function() {
-  console.log("You've clicked the button, well done.")
+var init = function() {
+  output = document.getElementById("output");
+}
+
+var addOutput = function() {
+  console.log("add");
+  var li = document.createElement("li");
+  li.innerText = "You've clicked the button, well done.";
+  output.appendChild(li);
+}
+
+var clearOutput = function() {
+  console.log("clear");
+  output.innerHTML = "";
 }
