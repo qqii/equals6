1e4532f4897f7bb8d8e23afc38a6c9a4e8a1dbda
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-01-31 13:35:53 +0000

    Testing linking README.md from index.html

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@ var init = function() {
   output = document.getElementById("output");
 }
 
+var appendOutput = function(text) {
+  var li = document.createElement("li");
+  li.innerText = text;
+  output.appendChild(li);
+}
+
 var addOutput = function() {
   console.log("add");
-  var li = document.createElement("li");
-  li.innerText = "You've clicked the button, well done.";
-  output.appendChild(li);
+  appendOutput((new Date()).toLocaleTimeString());
 }
 
 var clearOutput = function() {
