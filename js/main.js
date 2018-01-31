f3fe9f111f88272b76e08da9283d05e0abf7e1d3
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-01-31 14:12:57 +0000

    Search normal binary operations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 "use strict"
 var output;
-
-var init = function() {
-  output = document.getElementById("output");
-}
+// original question
+var range = [2, 3, 4, 5, 6, 7, 8, 9];
+// Basic binary operatons
+var add = (x, y) => x + y; add.toString = () => "+";
+var sub = (x, y) => x - y; sub.toString = () => "-";
+var mul = (x, y) => x * y; mul.toString = () => "*";
+var div = (x, y) => x / y; div.toString = () => "/";
+var binops = [add, sub, mul, div];
+// // Basic unary opeartions
+// var id = (x) => x;
+// var neg = (x) => (-x);
+// var sqrt = (x) => Math.sqrt(1);
 
 var appendOutput = function(text) {
   var li = document.createElement("li");
@@ -11,12 +19,32 @@ var appendOutput = function(text) {
   output.appendChild(li);
 }
 
-var addOutput = function() {
-  console.log("add");
-  appendOutput((new Date()).toLocaleTimeString());
+var search = function() {
+  range.forEach(
+    function(i) {
+      binops.forEach(
+        function(f) {
+          binops.forEach(
+            function(g) {
+              var m = g(f(i, i), i); // avoids eval and cleaner
+              if (m == 6) {
+                appendOutput(["("+i, f, i+")", g, i, "=", m].join(" "));  
+              } else {
+                appendOutput(["("+i, f, i+")", g, i, "=", m].join(" ")); 
+              }
+            }
+          );
+        }
+      );
+      
+    }
+  );
 }
 
 var clearOutput = function() {
-  console.log("clear");
   output.innerHTML = "";
+}
+
+var init = function() {
+  output = document.getElementById("output");
 }
